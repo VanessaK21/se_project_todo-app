bde7f5fee787ddd3f520154abd094e283aab4a7f
VK21 <Vanessa.k21@yahoo.com>
2025-08-25 14:11:25 -0500

    todo.js updates

--- components/Todo.js
+++ components/Todo.js
@@ -4,18 +4,16 @@ class Todo {
     this._templateElement = document.querySelector(selector);
   }
 
-   todoDeleteBtn()
-   todoDate ();
-
   _setEventListeners() {
     this._todoCheckboxEl.addEventListener("change", () => {
       this._data.completed = !this._data.completed;
     });
 
-    this._todoDeleteBtn.addEventListener("click", () )=> 
+    this._todoDeleteBtn.addEventListener("click", () => {
       this._todoElement.remove();
-
-    _generateDatesElement() {
+    });
+  }
+  _generateDatesElement() {
     this._dueDate = new Date(this._data.date);
     if (!isNaN(this._dueDate)) {
       this._todoDate.textContent = `Due: ${this._dueDate.toLocaleString(
@@ -26,34 +24,36 @@ class Todo {
           day: "numeric",
         }
       )}`;
-    };
     }
   }
-
-   this._todoElement = this._templateElement.content
+  getView() {
+    this._todoElement = this._templateElement.content
       .querySelector(".todo")
       .cloneNode(true);
 
-  _getView() {
     this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
     this._todoLabel = this._todoElement.querySelector(".todo__label");
     this._todoCheckboxEl.checked = this._data.completed;
     this._todoCheckboxEl.id = `todo-${this._data.id}`;
     this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
 
+    this._todoElement = this._templateElement.content
+      .querySelector(".todo")
+      .cloneNode(true);
+
     const todoNameEl = this._todoElement.querySelector(".todo__name");
 
     const todoDate = this._todoElement.querySelector(".todo__date");
     const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+    this._todoDeleteBtn = todoDeleteBtn;
+    this._todoDate = todoDate;
 
     todoNameEl.textContent = this._data.name;
 
-    this._generateCheckboxEl();
     this._setEventListeners();
     this._generateDatesElement();
 
     return this._todoElement;
-    this._todo.resetValidation();
   }
 }
 
